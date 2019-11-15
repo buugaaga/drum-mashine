323a7bbf3260908d5a129bdf0bc57e15f24ae97e
Your Name <you@example.com>
2019-11-15 14:40:21 +0500

    Updates

---
+++ precache-manifest.479fc480d9e25c51ec3d17a02322e683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d361c5e0919691a99fa1c3a8464aacd4",
+    "url": "/drum-mashine/index.html"
+  },
+  {
+    "revision": "c9ab58ab3a869e60ccb3",
+    "url": "/drum-mashine/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4367b7b29a70c3f9f8e9",
+    "url": "/drum-mashine/static/css/main.88b0d56e.chunk.css"
+  },
+  {
+    "revision": "c9ab58ab3a869e60ccb3",
+    "url": "/drum-mashine/static/js/2.5f9b1dbe.chunk.js"
+  },
+  {
+    "revision": "4367b7b29a70c3f9f8e9",
+    "url": "/drum-mashine/static/js/main.e0d2bc47.chunk.js"
+  },
+  {
+    "revision": "3c22a6270f65f1d6821e",
+    "url": "/drum-mashine/static/js/runtime-main.b8a2fe85.js"
+  }
+]);
